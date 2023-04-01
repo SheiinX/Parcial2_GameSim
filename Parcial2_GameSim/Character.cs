9f25bde204e10b5391f03ac2b2ff4cf45837cf21
SheiinX <sebastianfrancocatano@gmail.com>
2023-04-01 09:37:33 -0500

    Starting with the first class Character.cs

---
+++ Parcial2_GameSim/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_GameSim
+{
+    class Character
+    {
+        private string name = "";
+        private int hp = 0;
+        private int atk = 0;
+        private int def = 0;
+
+        public enum Clase
+        {
+            Human,
+            Beast,
+            Hybrid
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int HP { get => hp; set => hp = value; }
+        public int ATK { get => atk; set => atk = value; }
+        public int DEF { get => def; set => def = value; }
+    }
+}
