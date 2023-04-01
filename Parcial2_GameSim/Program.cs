ce9d64d454adc842704d151287fb30f5c45d1a59
SheiinX <sebastianfrancocatano@gmail.com>
2023-04-01 08:44:55 -0500

    Initial commit

---
+++ Parcial2_GameSim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parcial2_GameSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
